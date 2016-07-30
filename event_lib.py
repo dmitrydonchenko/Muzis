0d777158310242ebf8edb845878033b7a62a6490
Dmitry Donchenko <dmitrydonchenko92@gmail.com>
2016-07-31 00:29:32 +0300

    events lib changed

--- event_lib.py
+++ event_lib.py
@@ -18,8 +18,13 @@ def get_events_from_vk(artist_name, event_city):
             if city["title"] == event_city:
                 current_city_id = city["cid"]
     events = api.groups.search(q = artist_name, type = "event", country_id = 1, city_id = current_city_id)
-    for event in events:
-        if isinstance(event, dict):
-            print(event["name"])
+    #for event in events:
+    #    if isinstance(event, dict):
+    #       print(event["name"])
+    return events
+
+
+
+
 
 
