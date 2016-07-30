58079f8517e0f0c1146d954259d32bc00709eb7a
iron-phoenix <kluchikhin@gmail.com>
2016-07-31 00:26:57 +0300

    Parse string with new artists

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -3,17 +3,18 @@ import telebot
 from enum import Enum
 
 class BotStates(Enum):
-    AddingArtists = 0
-    GettingEvents = 1
-    GettingArtisEvents = 2
-    FindingSoulmates = 3
+    Waiting = 0
+    AddingArtists = 1
+    GettingEvents = 2
+    GettingArtisEvents = 3
+    FindingSoulmates = 4
 
 bot = telebot.TeleBot(config.token)
 
-greetings_str = 'Привет! Я бот, соединяющий души\n\n'
+bot_state = BotStates.Waiting
 
 help_string = []
-help_string.append(greetings_str)
+help_string.append('Привет! Я бот, соединяющий души\n\n')
 help_string.append('/start - начать общение с ботом;\n')
 help_string.append('/help - вывод справки;\n')
 help_string.append('/addArtists - добавить новых исполнителей;\n')
@@ -27,11 +28,18 @@ def greetings(message):
 
 @bot.message_handler(commands=["addArtists"])
 def add_artists(message):
+    global bot_state
     bot.send_message(message.chat.id, "Напиши мне через запятую список своих любимых исполнителей")
+    bot_state = BotStates.AddingArtists
 
-@bot.message_handler(content_types="text")
-def adding_artist(chat_id):
-    bot.send_message(message.chat.id, "Напиши мне через запятую список своих любимых исполнителей")
+@bot.message_handler(content_types=["text"])
+def handle_dialog(message):
+    global bot_state
+    if (bot_state == BotStates.AddingArtists):
+        bot.send_message(message.chat.id, 'Я запомнил следующих твоих любимых исполнителей:\n\n' + "".join(parse_artists(message.text)))
+
+def parse_artists(artists_string):
+    return [x.strip() + '\n' for x in artists_string.split(',')]
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
