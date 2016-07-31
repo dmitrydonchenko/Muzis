db723e6f4c48bc882895654d68e0f3e46042d742
iron-phoenix <kluchikhin@gmail.com>
2016-07-31 07:43:01 +0300

    Fix bug with database

--- chatbot/bot.py
+++ chatbot/bot.py
@@ -21,6 +21,7 @@ help_string.append('/start - начать общение с ботом;\n')
 help_string.append('/help - вывод справки;\n')
 help_string.append('/addArtists - добавить новых исполнителей;\n')
 help_string.append('/setLocation - добавить новых исполнителей;\n')
+help_string.append('/getPossibleEvents - куда пойти;\n')
 help_string.append('/checkinEvents - зачекиниться на события;\n')
 help_string.append('/getArtistEvents - вывод событий по исполнителю;\n')
 help_string.append('/findSoulmates - найти людей, которые идут на то же событие.\n')
@@ -55,12 +56,19 @@ def set_user_location(message):
 def checkin_events(message):
     bot.send_message(message.chat.id, 'Напиши через запятую, на какие из следующих событий ты хочешь пойти:')
 
-    events = bot_db.get_user_events(message.chat.id)
+    events = bot_db.get_user_possible_events(message.chat.id)
     for event in events:
         bot.send_message(message.chat.id, event.url)
 
     with shelve.open(config.bot_state_db) as db:
         db[message.chat.id] = BotStates.CheckinEvents
+
+@bot.message_handler(commands=["getPossibleEvents"])
+def get_possible_events(message):
+    for event in bot_db.get_user_possible_events(str(message.chat.id)):
+        if event != None:
+            bot.send_message(message.chat.id, event.url)
+
 
 @bot.message_handler(content_types=["text"])
 def handle_dialog(message):
@@ -73,7 +81,7 @@ def handle_dialog(message):
         for artist in artists:
             db_save_artist(message.chat.id, artist)
 
-        bot.send_message(message.chat.id, 'Я запомнил следующих твоих любимых исполнителей:\n\n' + "".join(artists))
+        bot.send_message(message.chat.id, 'Я запомнил следующих твоих любимых исполнителей:\n\n' + "\n".join(artists))
     elif (bot_state == BotStates.SetLocation):
         # with shelve.open(config.location_db) as db:
         #     db[message.chat.id] = message.text
@@ -84,15 +92,16 @@ def handle_dialog(message):
     set_bot_state(message.chat.id, BotStates.Waiting)
 
 def parse_artists(artists_string):
-    return [x.strip().lower() + '\n' for x in artists_string.split(',')]
+    return [x.strip().lower() for x in artists_string.split(',')]
 
 def db_save_artist(user_id, artist):
     bot_db.insert_artist(artist)
+    bot_db.insert_user_artist(str(user_id), artist)
     location = get_location(user_id)
     events = event_lib.get_events_from_vk(artist, location)
     for event in events:
         url = 'vk.com/' + event['screen_name']
-        bot_db.insert_event(event['name'], url, location)
+        bot_db.insert_event(event['name'], url, location, artist)
 
 def set_bot_state(user_id, bot_state):
     with shelve.open(config.bot_state_db) as db:
